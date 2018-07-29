96678447a20d63c77e406d468f040d2e777cf6c4
Marko Milojevic <milojevicmarko92@gmail.com>
2018-07-29 16:56:42 +0200

    First commit.

---
+++ RomanNumerals/Declarative/IntExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RomanNumerals.Declarative
+{
+    public static class IntExtensions
+    {
+        public static IEnumerable<int> GetWeightedDigits(this int number) =>
+            number
+                .GetDigits()
+                .Zip(number.GetWeights(), (digit, weight) => digit * weight)
+                .Where(weightedDigit => weightedDigit != 0);
+
+        public static IEnumerable<int> GetDigits(this int number) =>
+            number
+                .ToString()
+                .Select(c => int.Parse(c.ToString()));
+
+        private static IEnumerable<int> GetWeights(this int number) =>
+            Sequences
+                .PowerOf10
+                .Take(number.GetNumberOfDigits())
+                .Reverse();
+
+        public static int GetNumberOfDigits(this int number) =>
+            number
+                .ToString()
+                .Length;
+    }
+}
---
+++ RomanNumerals/Declarative/Sequences.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace RomanNumerals.Declarative
+{
+    public static class Sequences
+    {
+        public static IEnumerable<int> PowerOf10 { get; }
+
+        static Sequences() =>
+            PowerOf10 = new[] { 1, 10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000, 1_000_000_000 };
+    }
+}
