5eecc36106e95d820517c1b38c9d3cd866971f1e
Marko Milojevic <milojevicmarko92@gmail.com>
2018-08-07 10:26:12 +0200

    Commit.

--- RomanNumerals/Declarative/IntExtensions.cs
+++ RomanNumerals/Declarative/IntExtensions.cs
@@ -8,13 +8,16 @@ namespace RomanNumerals.Declarative
         public static IEnumerable<int> GetWeightedDigits(this int number) =>
             number
                 .GetDigits()
-                .Zip(number.GetWeights(), (digit, weight) => digit * weight)
+                .MultiplyWith(number.GetWeights())
                 .Where(weightedDigit => weightedDigit != 0);
 
         public static IEnumerable<int> GetDigits(this int number) =>
             number
                 .ToString()
                 .Select(c => int.Parse(c.ToString()));
+
+        private static IEnumerable<int> MultiplyWith(this IEnumerable<int> source, IEnumerable<int> factors) =>
+            source.Zip(factors, (x, y) => x * y);
 
         private static IEnumerable<int> GetWeights(this int number) =>
             Sequences
