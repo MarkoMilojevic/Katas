cd908ce964664d2f7c35e96ace537678a6bb537d
Marko Milojevic <milojevicmarko92@gmail.com>
2018-08-22 01:47:47 +0200

    Commit.

--- Diamond/Diamond.cs
+++ Diamond/Diamond.cs
@@ -6,18 +6,19 @@ namespace Diamond
     public class Diamond
     {
         public static string Create(char letter) =>
+            Create(DiamondSize(letter));
+
+        private static string Create(int diamondSize) =>
             Enumerable
-                .Range(0, DiamondSize(letter))
-                .Select(index => CreateRow(letter, index))
+                .Range(0, diamondSize)
+                .Select(index => CreateRow(index, diamondSize))
                 .Join(Environment.NewLine);
 
         private static int DiamondSize(char letter) =>
             2 * (letter - 'A') + 1;
 
-        private static string CreateRow(char diamondLetter, int rowIndex)
+        private static string CreateRow(int rowIndex, int diamondSize)
         {
-            int diamondSize = DiamondSize(diamondLetter);
-
             char[] row = new string('-', diamondSize).ToCharArray();
 
             int charIndex = CharIndex(rowIndex, diamondSize);
