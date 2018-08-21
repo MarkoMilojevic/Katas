6817a69f7a7ac46a085654983312e603026f5b2b
Marko Milojevic <milojevicmarko92@gmail.com>
2018-08-21 02:35:54 +0200

    Commit.

---
+++ Diamond.UnitTests/DiamondTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+namespace Diamond.UnitTests
+{
+    public class DiamondTests
+    {
+        [Theory]
+        [InlineData('A', 1)]
+        [InlineData('B', 3)]
+        [InlineData('C', 5)]
+        [InlineData('D', 7)]
+        [InlineData('Z', 51)]
+        public void NumberOfRowsInDiamondTest(char letter, int expectedNumberOfRows)
+        {
+            string diamond = Diamond.CreateFor(letter);
+
+            int numberOfRows = diamond.Split(new[] { Environment.NewLine }, StringSplitOptions.None).Length;
+
+            Assert.Equal(expectedNumberOfRows, numberOfRows);
+        }
+
+        [Theory]
+        [InlineData('A', 1)]
+        [InlineData('B', 3)]
+        [InlineData('C', 5)]
+        [InlineData('D', 7)]
+        [InlineData('Z', 51)]
+        public void NumberOfColumnsInDiamondTest(char letter, int expectedNumberOfColumns)
+        {
+            string diamond = Diamond.CreateFor(letter);
+
+            string[] rows = diamond.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            foreach (string row in rows)
+            {
+                int numberOfColumns = row.Length;
+
+                Assert.Equal(expectedNumberOfColumns, numberOfColumns);
+            }
+        }
+
+        [Theory]
+        [InlineData('A', 0, 'A')]
+        [InlineData('B', 0, 'A')]
+        [InlineData('B', 1, 'B')]
+        [InlineData('B', 2, 'A')]
+        [InlineData('C', 0, 'A')]
+        [InlineData('C', 1, 'B')]
+        [InlineData('C', 2, 'C')]
+        [InlineData('C', 3, 'B')]
+        [InlineData('C', 4, 'A')]
+        public void RowContainsCorrectLetter(char letter, int rowIndex, char expectedLetter)
+        {
+            string diamond = Diamond.CreateFor(letter);
+
+            string[] rows = diamond.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            string row = rows[rowIndex];
+
+            Assert.True(row.Contains(expectedLetter));
+        }
+
+        [Theory]
+        [InlineData('A', 0, 'A', 0, 0)]
+        [InlineData('B', 0, 'A', 1, 1)]
+        [InlineData('B', 1, 'B', 0, 2)]
+        [InlineData('B', 2, 'A', 1, 1)]
+        [InlineData('C', 0, 'A', 2, 2)]
+        [InlineData('C', 1, 'B', 1, 3)]
+        [InlineData('C', 2, 'C', 0, 4)]
+        [InlineData('C', 3, 'B', 1, 3)]
+        [InlineData('C', 4, 'A', 2, 2)]
+        public void LettersAreInCorrectColumns(char letter, int rowIndex, char expectedLetter, int firstColumnIndex, int secondColumnIndex)
+        {
+            string diamond = Diamond.CreateFor(letter);
+
+            string[] rows = diamond.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            char[] row = rows[rowIndex].ToCharArray();
+
+            for (int i = 0; i < row.Length; i++)
+            {
+                if (i == firstColumnIndex || i == secondColumnIndex)
+                    Assert.Equal(expectedLetter, row[i]);
+                else
+                    Assert.Equal('-', row[i]);
+            }
+        }
+
+        [Fact]
+        public void Z_DiamondIsCorrect()
+        {
+            string expectedDiamond = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "z_diamond.txt"));
+
+            Assert.Equal(expectedDiamond, Diamond.CreateFor('Z'));
+        }
+    }
+}
---
+++ Diamond/Diamond.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Diamond
+{
+    public class Diamond
+    {
+        public static string CreateFor(char letter) =>
+            Enumerable
+                .Range(0, DiamondSizeFor(letter))
+                .Select(index => CreateRow(letter, index))
+                .Join(Environment.NewLine);
+
+        private static int DiamondSizeFor(char letter) =>
+            2 * (letter - 'A' + 1) - 1;
+
+        private static string CreateRow(char letter, int rowIndex)
+        {
+            int diamondSize = DiamondSizeFor(letter);
+
+            char[] row = new string('-', diamondSize).ToCharArray();
+
+            char charForCurrentRow = CharAt(rowIndex, diamondSize);
+            int columnIndex = CharColumnIndexGiven(rowIndex, diamondSize);
+
+            row[columnIndex] = charForCurrentRow;
+            row[row.Length - 1 - columnIndex] = charForCurrentRow;
+
+            return new string(row);
+        }
+
+        private static char CharAt(int rowIndex, int diamondSize) =>
+            (char)('A' + CharIndexGiven(rowIndex, diamondSize));
+
+        private static int CharColumnIndexGiven(int rowIndex, int diamondSize) =>
+            MiddleColumnIndexFor(diamondSize) - CharIndexGiven(rowIndex, diamondSize);
+
+        private static int CharIndexGiven(int rowIndex, int diamondSize) =>
+            rowIndex <= MiddleColumnIndexFor(diamondSize)
+                ? rowIndex
+                : (diamondSize - 1) - rowIndex;
+
+        private static int MiddleColumnIndexFor(int diamondSize) =>
+            (diamondSize - 1) / 2;
+    }
+}
