9d853c5f5eece8ea55bcf28f8cece3c5b84b59ee
Marko Milojevic <milojevicmarko92@gmail.com>
2018-08-21 21:18:45 +0200

    Commit.

--- Diamond/Diamond.cs
+++ Diamond/Diamond.cs
@@ -7,22 +7,22 @@ namespace Diamond
     {
         public static string CreateFor(char letter) =>
             Enumerable
-                .Range(0, DiamondSizeFor(letter))
-                .Select(index => CreateRowFor(letter, index))
+                .Range(0, DiamondSize(letter))
+                .Select(index => CreateRow(letter, index))
                 .Join(Environment.NewLine);
 
-        private static int DiamondSizeFor(char letter) =>
+        private static int DiamondSize(char letter) =>
             2 * (letter - 'A' + 1) - 1;
 
-        private static string CreateRowFor(char diamondLetter, int rowIndex)
+        private static string CreateRow(char diamondLetter, int rowIndex)
         {
-            int diamondSize = DiamondSizeFor(diamondLetter);
+            int diamondSize = DiamondSize(diamondLetter);
 
             char[] row = new string('-', diamondSize).ToCharArray();
 
-            int charIndex = CharIndexGiven(rowIndex, diamondSize);
-            char charForCurrentRow = CharFor(charIndex);
-            int middleColumnIndex = MiddleColumnIndexFor(diamondSize);
+            int charIndex = CharIndex(rowIndex, diamondSize);
+            char charForCurrentRow = CharAt(charIndex);
+            int middleColumnIndex = MiddleColumnIndex(diamondSize);
 
             row[middleColumnIndex - charIndex] = charForCurrentRow;
             row[middleColumnIndex + charIndex] = charForCurrentRow;
@@ -30,18 +30,18 @@ namespace Diamond
             return new string(row);
         }
 
-        private static int CharIndexGiven(int rowIndex, int diamondSize) =>
-            rowIndex <= MiddleRowIndexFor(diamondSize)
+        private static int CharIndex(int rowIndex, int diamondSize) =>
+            rowIndex <= MiddleRowIndex(diamondSize)
                 ? rowIndex
                 : diamondSize - 1 - rowIndex;
 
-        private static char CharFor(int charIndex) =>
+        private static char CharAt(int charIndex) =>
             (char)('A' + charIndex);
 
-        private static int MiddleRowIndexFor(int diamondSize) =>
+        private static int MiddleRowIndex(int diamondSize) =>
             (diamondSize - 1) / 2;
 
-        private static int MiddleColumnIndexFor(int diamondSize) =>
-            MiddleRowIndexFor(diamondSize);
+        private static int MiddleColumnIndex(int diamondSize) =>
+            MiddleRowIndex(diamondSize);
     }
 }
