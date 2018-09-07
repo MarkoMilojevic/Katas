b01fc019f0173d3f30124e62a56d584253fc67f6
Marko Milojevic <milojevicmarko92@gmail.com>
2018-09-07 14:04:22 +0200

    Update MarsRover.cs

--- MarsRover/MarsRover.cs
+++ MarsRover/MarsRover.cs
@@ -9,7 +9,21 @@ namespace MarsRover
         public MarsRover(Position position) =>
             Position = position ?? throw new ArgumentNullException(nameof(position));
 
-        public MarsRover Execute(string instructions) =>
-            new MarsRover(Position.Transform(instructions));
+        public MarsRover Execute(string instructions)
+        {
+            Position currentPosition = Position;
+
+            instructions = instructions.ToLower();
+            foreach (char instruction in instructions)
+            {
+                Position newPosition = currentPosition.Transform(instruction);
+                if (newPosition == currentPosition)
+                    break;
+
+                currentPosition = newPosition;
+            }
+
+            return new MarsRover(currentPosition);
+        }
     }
 }
