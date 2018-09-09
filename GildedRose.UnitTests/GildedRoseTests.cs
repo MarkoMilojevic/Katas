70c88e7c3a6eaa4c53af7da437120f07bfc81cad
Marko Milojevic <milojevicmarko92@gmail.com>
2018-09-09 02:58:30 +0200

    Commit.

---
+++ GildedRose.UnitTests/GildedRoseTests.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace GildedRose.UnitTests
+{
+    public class GildedRoseTests
+    {
+        [Fact]
+        public void EndOfDayLowersItemsQualityAndSellIn()
+        {
+            const int quality = 10;
+            const int sellIn = 10;
+
+            var items = new List<Item> { new Item { Name = "Item", SellIn = sellIn, Quality = quality } };
+
+            var gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+
+            Assert.Equal(quality - 1, items[0].Quality);
+            Assert.Equal(sellIn - 1, items[0].SellIn);
+        }
+
+        [Fact]
+        public void QualityDegradesTwiceAsFastWhenSellDatePasses()
+        {
+            const int quality = 10;
+            const int sellIn = 0;
+
+            var items = new List<Item> { new Item { Name = "Item", SellIn = sellIn, Quality = quality } };
+
+            var gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+
+            Assert.Equal(quality - 2, items[0].Quality);
+        }
+
+        [Fact]
+        public void QualityOfAnItemIsNeverNegative()
+        {
+            const int quality = 0;
+            const int sellIn = 0;
+
+            var items = new List<Item> { new Item { Name = "Item", SellIn = sellIn, Quality = quality } };
+
+            var gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+
+            Assert.Equal(quality, items[0].Quality);
+        }
+
+        [Fact]
+        public void AgedBrieIncreasesInQualityTheOlderItGets()
+        {
+            const int quality = 10;
+            const int sellIn = 10;
+
+            var items = new List<Item> { new Item { Name = GildedRose.AgedBrie, SellIn = sellIn, Quality = quality } };
+
+            var gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+
+            Assert.Equal(quality + 1, items[0].Quality);
+        }
+
+        [Fact]
+        public void AgedBrieIncreasesInQualityTwiceAsFastWhenSellDatePasses()
+        {
+            const int quality = 10;
+            const int sellIn = 0;
+
+            var items = new List<Item> { new Item { Name = GildedRose.AgedBrie, SellIn = sellIn, Quality = quality } };
+
+            var gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+
+            Assert.Equal(quality + 2, items[0].Quality);
+        }
+
+        [Fact]
+        public void BackstagePassesIncreasesInQualityTheOlderItGets()
+        {
+            const int quality = 10;
+            const int sellIn = 20;
+
+            var items = new List<Item> { new Item { Name = GildedRose.BackstagePasses, SellIn = sellIn, Quality = quality } };
+
+            var gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+
+            Assert.Equal(quality + 1, items[0].Quality);
+        }
+
+        [Fact]
+        public void BackstagePassesIncreasesInQualityTwiceAsFastTenDaysBeforeSellDate()
+        {
+            const int quality = 10;
+            const int sellIn = 10;
+
+            var items = new List<Item> { new Item { Name = GildedRose.BackstagePasses, SellIn = sellIn, Quality = quality } };
+
+            var gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+
+            Assert.Equal(quality + 2, items[0].Quality);
+        }
+
+        [Fact]
+        public void BackstagePassesIncreasesInQualityThreeTimesAsFastFiveDaysBeforeSellDate()
+        {
+            const int quality = 10;
+            const int sellIn = 5;
+
+            var items = new List<Item> { new Item { Name = GildedRose.BackstagePasses, SellIn = sellIn, Quality = quality } };
+
+            var gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+
+            Assert.Equal(quality + 3, items[0].Quality);
+        }
+
+        [Fact]
+        public void BackstagePassesQualityDropsToZeroWhenSellDatePasses()
+        {
+            const int quality = 10;
+            const int sellIn = 0;
+
+            var items = new List<Item> { new Item { Name = GildedRose.BackstagePasses, SellIn = sellIn, Quality = quality } };
+
+            var gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+
+            Assert.Equal(0, items[0].Quality);
+        }
+
+        [Fact]
+        public void SulfurasNeverHasToBeSoldAndDoesntLoseQualityAsTimePasses()
+        {
+            const int quality = 80;
+            const int sellIn = 10;
+
+            var items = new List<Item> { new Item { Name = GildedRose.Sulfuras, SellIn = sellIn, Quality = quality } };
+
+            var gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+
+            Assert.Equal(quality, items[0].Quality);
+            Assert.Equal(sellIn, items[0].SellIn);
+        }
+
+        [Theory]
+        [InlineData(GildedRose.AgedBrie, 10)]
+        [InlineData(GildedRose.AgedBrie, 0)]
+        [InlineData(GildedRose.BackstagePasses, 20)]
+        [InlineData(GildedRose.BackstagePasses, 10)]
+        [InlineData(GildedRose.BackstagePasses, 5)]
+        public void ItemCannotHaveHigherQualityThanFiftyUnlessSulfuras(string itemName, int sellIn)
+        {
+            const int maxQuality = 50;
+
+            var items = new List<Item> { new Item { Name = itemName, SellIn = sellIn, Quality = maxQuality } };
+
+            var gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+
+            Assert.Equal(maxQuality, items[0].Quality);
+        }
+    }
+}
---
+++ GildedRose/GildedRose.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRose
+{
+    public class GildedRose
+    {
+        public const string AgedBrie = "Aged Brie";
+        public const string BackstagePasses = "Backstage passes to a TAFKAL80ETC concert";
+        public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+
+        private IList<Item> Items { get; }
+
+        public GildedRose(IList<Item> items) =>
+            Items = items;
+
+        public void UpdateQuality()
+        {
+            for (int i = 0; i < Items.Count; i++)
+            {
+                if (Items[i].Name != AgedBrie && Items[i].Name != BackstagePasses)
+                {
+                    if (Items[i].Quality > 0)
+                    {
+                        if (Items[i].Name != Sulfuras)
+                        {
+                            Items[i].Quality = Items[i].Quality - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
+
+                        if (Items[i].Name == BackstagePasses)
+                        {
+                            if (Items[i].SellIn < 11)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+
+                            if (Items[i].SellIn < 6)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (Items[i].Name != Sulfuras)
+                {
+                    Items[i].SellIn = Items[i].SellIn - 1;
+                }
+
+                if (Items[i].SellIn < 0)
+                {
+                    if (Items[i].Name != AgedBrie)
+                    {
+                        if (Items[i].Name != BackstagePasses)
+                        {
+                            if (Items[i].Quality > 0)
+                            {
+                                if (Items[i].Name != Sulfuras)
+                                {
+                                    Items[i].Quality = Items[i].Quality - 1;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                        }
+                    }
+                    else
+                    {
+                        if (Items[i].Quality < 50)
+                        {
+                            Items[i].Quality = Items[i].Quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
