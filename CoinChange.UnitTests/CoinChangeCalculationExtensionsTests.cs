7890a9006376e63e31e9ec4606b6827dc5edc4de
Marko Milojevic <marko.milojevic@nultien.rs>
2018-09-10 17:09:58 +0200

    Commit.

--- CoinChange.UnitTests/CoinChangeCalculationExtensionsTests.cs
+++ CoinChange.UnitTests/CoinChangeCalculationExtensionsTests.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using CoinChange.Functional;
+using FunctionalExtensions;
 using Xunit;
 
 namespace CoinChange.UnitTests
--- CoinChange/CoinChangeCalculationExtensions.cs
+++ CoinChange/CoinChangeCalculationExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using CoinChange.Functional;
+using FunctionalExtensions;
 
 namespace CoinChange
 {
