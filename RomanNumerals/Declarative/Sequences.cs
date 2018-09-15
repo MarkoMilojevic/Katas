3be3d915d21bbf8f2784996ec431e98e08feaa1a
Marko Milojevic <milojevicmarko92@gmail.com>
2018-09-16 01:30:02 +0200

    Commit.

--- RomanNumerals/Declarative/Sequences.cs
+++ RomanNumerals/Declarative/Sequences.cs
@@ -4,9 +4,7 @@ namespace RomanNumerals.Declarative
 {
     public static class Sequences
     {
-        public static IEnumerable<int> PowerOf10 { get; }
-
-        static Sequences() =>
-            PowerOf10 = new[] { 1, 10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000, 1_000_000_000 };
+        public static IEnumerable<int> PowerOf10 { get; } = 
+            new[] { 1, 10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000, 1_000_000_000 };
     }
 }
