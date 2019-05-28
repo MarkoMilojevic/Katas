cae98c5f922344fc8e2a43cc105f4719de19d7eb
Marko Milojevic <milojevicmarko92@gmail.com>
2019-05-28 19:51:07 +0200

    Refactor.

--- ClockWiseTreeSearch.UnitTests/MatrixExtensionsTests.cs
+++ ClockWiseTreeSearch.UnitTests/MatrixExtensionsTests.cs
@@ -6,30 +6,6 @@ namespace ClockWiseTreeSearch.UnitTests
     public class MatrixExtensionsTests
     {
         [Fact]
-        public void AlternateRowsVerticallyTest()
-        {
-            int[][] matrix = new[]
-            {
-                new[] { 1 },
-                new[] { 2, 3 },
-                new[] { 4, 5, 6, 7 },
-                new[] { 8, 9, 10, 11, 12, 13, 14, 15 }
-            };
-
-            int[][] actual = matrix.AlternateRowsVertically();
-
-            int[][] expected = new[]
-            {
-                new[] { 1 },
-                new[] { 8, 9, 10, 11, 12, 13, 14, 15 },
-                new[] { 2, 3 },
-                new[] { 4, 5, 6, 7 }
-            };
-
-            AssertMatrix(actual, expected);
-        }
-
-        [Fact]
         public void ReverseRowsInBottomHalfTest()
         {
             int[][] matrix = new[]
@@ -48,6 +24,30 @@ namespace ClockWiseTreeSearch.UnitTests
                 new[] { 2, 3 },
                 new[] { 7, 6, 5, 4 },
                 new[] { 15, 14, 13, 12, 11, 10, 9, 8 }
+            };
+
+            AssertMatrix(actual, expected);
+        }
+
+        [Fact]
+        public void AlternateRowsVerticallyTest()
+        {
+            int[][] matrix = new[]
+            {
+                new[] { 1 },
+                new[] { 2, 3 },
+                new[] { 4, 5, 6, 7 },
+                new[] { 8, 9, 10, 11, 12, 13, 14, 15 }
+            };
+
+            int[][] actual = matrix.AlternateRowsVertically();
+
+            int[][] expected = new[]
+            {
+                new[] { 1 },
+                new[] { 8, 9, 10, 11, 12, 13, 14, 15 },
+                new[] { 2, 3 },
+                new[] { 4, 5, 6, 7 }
             };
 
             AssertMatrix(actual, expected);
--- ClockWiseTreeSearch/MatrixExtensions.cs
+++ ClockWiseTreeSearch/MatrixExtensions.cs
@@ -5,6 +5,17 @@ namespace ClockWiseTreeSearch
 {
     public static class MatrixExtensions
     {
+        public static T[][] ReverseRowsInBottomHalf<T>(this T[][] levels)
+        {
+            if (levels == null)
+                return new T[0][];
+
+            return Enumerable
+                    .Range(0, levels.Length)
+                    .Select(i => i < (levels.Length + 1) / 2 ? levels[i] : levels[i].AsEnumerable().Reverse().ToArray())
+                    .ToArray();
+        }
+
         public static T[][] AlternateRowsVertically<T>(this T[][] levels)
         {
             if (levels == null)
@@ -14,17 +25,6 @@ namespace ClockWiseTreeSearch
                     .Length
                     .AlternateIndexes()
                     .Select(i => levels[i])
-                    .ToArray();
-        }
-
-        public static T[][] ReverseRowsInBottomHalf<T>(this T[][] levels)
-        {
-            if (levels == null)
-                return new T[0][];
-
-            return Enumerable
-                    .Range(0, levels.Length)
-                    .Select(i => i < (levels.Length + 1) / 2 ? levels[i] : levels[i].AsEnumerable().Reverse().ToArray())
                     .ToArray();
         }
     }
