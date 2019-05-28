fd29e6f7df710bd2556c7659a148d41f432b4b43
Marko Milojevic <v-mamilo@microsoft.com>
2019-05-28 12:38:28 +0200

    Refactor ClockWiseTreeSearch.

---
+++ ClockWiseTreeSearch.UnitTests/Asserts.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace ClockWiseTreeSearch.UnitTests
+{
+    public static class Asserts
+    {
+        public static void AssertMatrix(int[][] actual, int[][] expected)
+        {
+            Assert.Equal(expected.Length, actual.Length);
+
+            for (int i = 0; i < actual.Length; i++)
+            {
+                Assert.Equal(expected[i].Length, actual[i].Length);
+
+                Assert.Equal(expected[i], actual[i]);
+            }
+        }
+    }
+}
---
+++ ClockWiseTreeSearch.UnitTests/MatrixExtensionsTests.cs
@@ -0,0 +1,56 @@
+﻿using Xunit;
+using static ClockWiseTreeSearch.UnitTests.Asserts;
+
+namespace ClockWiseTreeSearch.UnitTests
+{
+    public class MatrixExtensionsTests
+    {
+        [Fact]
+        public void AlternateVerticallyTest()
+        {
+            int[][] matrix = new[]
+            {
+                new[] { 1 },
+                new[] { 2, 3 },
+                new[] { 4, 5, 6, 7 },
+                new[] { 8, 9, 10, 11, 12, 13, 14, 15 }
+            };
+
+            int[][] actual = matrix.AlternateVertically();
+
+            int[][] expected = new[]
+            {
+                new[] { 1 },
+                new[] { 8, 9, 10, 11, 12, 13, 14, 15 },
+                new[] { 2, 3 },
+                new[] { 4, 5, 6, 7 }
+            };
+
+            AssertMatrix(actual, expected);
+        }
+
+        [Fact]
+        public void AlternateReverseTest()
+        {
+            int[][] matrix = new[]
+            {
+                new[] { 1 },
+                new[] { 2, 3 },
+                new[] { 4, 5, 6, 7 },
+                new[] { 8, 9, 10, 11, 12, 13, 14, 15 }
+            };
+
+            int[][] actual = matrix.AlternateReverse();
+
+            int[][] expected = new[]
+            {
+                new[] { 1 },
+                new[] { 3, 2 },
+                new[] { 4, 5, 6, 7 },
+                new[] { 15, 14, 13, 12, 11, 10, 9, 8 }
+            };
+
+            AssertMatrix(actual, expected);
+        }
+    }
+}
